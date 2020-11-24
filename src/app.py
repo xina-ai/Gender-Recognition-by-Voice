21dfa16ba1a65d0925f9b99afef76da7cc90bb66
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-11-24 12:44:02 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,33 +6,46 @@ import pickle
 import numpy as np
 from flask import Flask, request
 from utils import extract_features
+from pydub import AudioSegment
+import warnings
+warnings.filterwarnings('ignore')
+import base64
+import io
+import os
 
 app = Flask(__name__)
 
 MODEL = None
 
 
-def audio_prediction(audio_file):
+def audio_prediction(audio_data):
+    data = base64.b64decode(audio_data)
+    AudioSegment.from_file(io.BytesIO(data)).low_pass_filter(8000).set_frame_rate(48000).export(
+        "tmp.mp3", format="mp3", bitrate='64k')
     feats = extract_features(
-        audio_file, mel=True, mfcc=True, chroma=True, contrast=True)
+        'tmp.mp3', mel=True, mfcc=True, chroma=True, contrast=True)
     scaler = pickle.load(open(config.SCALAR_PATH, 'rb'))
     X = scaler.transform(feats.reshape(1, -1))
     pred = MODEL.predict_proba(X)
+    try:
+        os.remove('tmp.mp3')
+    except:
+        pass
     return pred[0][1]
 
 
-@app.route("/predict")
+@app.route("/predict", METHODS=['POST'])
 def predict():
-    audio_file = request.args.get("audio_file")
+    audio_data = request.get_json()['audio']
     start_time = time.time()
-    male_prediction = audio_prediction(audio_file)
+    male_prediction = audio_prediction(audio_data)
     female_prediction = 1 - male_prediction
     response = {}
     response["response"] = {
         "male": str(male_prediction),
         "female": str(female_prediction),
-        "audio_file": str(audio_file),
         "time_taken": str(time.time() - start_time),
+        'prediction': 'male' if male_prediction > female_prediction else 'female'
     }
     return flask.jsonify(response)
 
